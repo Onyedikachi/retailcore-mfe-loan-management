db46252470c0015de08d4b172c9cc9f4252bf8f1
Shalom Ijeoma Mathew <codabae@Shaloms-MacBook-Pro-2.local>
2023-07-18 17:38:54 +0100

    NextedDropdown implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,25 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import AppRoutes from './AppRoutes';
 import { GlobalTheme } from '@app/utils';
 import Accordion from './components/atoms/Accordion'
-import {Chip} from './components/atoms/Chip'
+import { Chip } from './components/atoms/Chip'
 import NestedDropdown from './components/atoms/NestedDropdown'
+import AddIcon from '@mui/icons-material/Add';
 
 
 
 const App = () => {
-   return (
-      <ThemeProvider theme={GlobalTheme}>
-         <CssBaseline />
-         <AppRoutes />
-         <Accordion accordionLabels={['Label 1', 'Label 2']}>
+  return (
+    <ThemeProvider theme={GlobalTheme}>
+      <CssBaseline />
+      <AppRoutes />
+      <Accordion accordionLabels={['Label 1', 'Label 2']}>
         <Chip />
         <Chip />
       </Accordion>
 
-      <NestedDropdown />
+      <NestedDropdown/>      
       </ThemeProvider>
-   );
+  );
 };
 
 export default App;
