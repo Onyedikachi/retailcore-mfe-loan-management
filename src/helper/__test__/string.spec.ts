4a0d2f2495c64a25dcd3f08d8bb9bf273e93a9e0
Shalom Ijeoma Mathew <codabae@Shaloms-MacBook-Pro-2.local>
2023-08-02 20:33:21 +0100

    added test to helper

--- src/helper/__test__/string.spec.ts
+++ src/helper/__test__/string.spec.ts
@@ -1,7 +1,43 @@
-import { capitalizeString } from '../string';
+import { capitalizeString, compareString, stringContains } from '../string';
 
 describe('String Helper', () => {
-   it('<capitalizeString> should capitalize the first Letter of a sentence', () => {
+   it('<capitalizeString> should capitalize the first letter of a sentence', () => {
       expect(capitalizeString('capital')).toBe('Capital');
+   });
+
+   it('<capitalizeString> should not change a sentence with already capitalized first letter', () => {
+      expect(capitalizeString('Capital')).toBe('Capital');
+   });
+
+   it('<capitalizeString> should capitalize the first letter of a single character', () => {
+      expect(capitalizeString('a')).toBe('A');
+   });
+
+   it('<compareString> should return true for two equal strings', () => {
+      expect(compareString('hello', 'hello')).toBe(true);
+   });
+
+   it('<compareString> should return true for two equal strings with different case', () => {
+      expect(compareString('Hello', 'HELLO')).toBe(true);
+   });
+
+   it('<compareString> should return false for two different strings', () => {
+      expect(compareString('hello', 'world')).toBe(false);
+   });
+
+   it('<stringContains> should return true for substring present in the string', () => {
+      expect(stringContains('hello', 'ell')).toBe(true);
+   });
+
+   it('<stringContains> should return true for substring present in the string with different case', () => {
+      expect(stringContains('Hello', 'ell')).toBe(true);
+   });
+
+   it('<stringContains> should return false for substring not present in the string', () => {
+      expect(stringContains('hello', 'abc')).toBe(false);
+   });
+
+   it('<stringContains> should return true for an empty substring', () => {
+      expect(stringContains('hello', '')).toBe(true);
    });
 });
--- src/helper/currency-helper.ts
+++ src/helper/currency-helper.ts
@@ -1,40 +1,47 @@
 import { isNullish } from '.';
 
 export const currencyToNumber = (currency: string) => {
-   return typeof currency === 'number' ? currency : parseFloat(currency.replace(/,/g, '') ?? 0);
-};
-
-export const currencyInputFormatter = (inputValue: string) => {
+   const cleanedCurrency = currency.replace(/,/g, '').trim();
+    if (isNullish(cleanedCurrency) || cleanedCurrency === '') {
+     return 0;
+   }
+ 
+   return parseFloat(cleanedCurrency);
+ };
+ 
+ export const currencyInputFormatter = (inputValue: string) => {
    const rawValue = inputValue.replace(/[^\d.]/g, '');
    const parts = rawValue.split('.');
-
+ 
    const integerPart = Number(parts[0] || 0).toLocaleString();
    const decimalPart = !isNullish(parts[1]) ? `.${parts[1].substring(0, 2)}` : '';
-
+ 
    return {
-      parts,
-      originalValue: inputValue,
-      hasDecimal: !!parts[1],
-      currency: `${integerPart}${decimalPart}`,
-      integerPart,
-      decimalPart,
+     parts,
+     originalValue: inputValue,
+     hasDecimal: !!parts[1],
+     currency: `${integerPart}${decimalPart}`,
+     integerPart,
+     decimalPart,
    };
-};
-
-export const percentageInputFormatter = (inputValue: string) => {
-   let value = inputValue.replace(/[^\d.]/g, '');
+ };
+ 
+ export const percentageInputFormatter = (inputValue: string) => {
+   let value = inputValue.replace(/[^\d.-]/g, ''); 
    value = Number(value) <= 100 ? value : value.substring(0, value.length - 1);
+ 
    const parts = value.split('.');
-   const integerPart = `${parts[0]}`;
-   const decimalPart = !isNullish(parts[1]) ? `.${parts[1].substring(0, 2)}` : '';
-
+   const integerPart = parts[0] ? `${parts[0]}` : '0';
+ 
+   let decimalPart = parts[1] !== undefined ? `.${parts[1].padEnd(1, '0')}` : '.0';
+ 
    return {
-      parts,
-      originalValue: inputValue,
-      hasDecimal: !!parts[1],
-      percentage: `${integerPart}${decimalPart}%`,
-      ratio: `${integerPart}${decimalPart}`,
-      integerPart,
-      decimalPart,
+     parts,
+     originalValue: inputValue,
+     hasDecimal: parts[1] !== undefined,
+     percentage: `${integerPart}${decimalPart}%`,
+     ratio: `${integerPart}${decimalPart}`,
+     integerPart,
+     decimalPart,
    };
-};
+ };
