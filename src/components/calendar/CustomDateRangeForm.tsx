44000bc6f7cb6a20500154f030c0be90166f25f2
OmololaOlusanya <faroyinomolola@gmail.com>
2023-10-07 15:07:20 +0100

    fix: added validation for the custom range form input

--- src/components/calendar/CustomDateRangeForm.tsx
+++ src/components/calendar/CustomDateRangeForm.tsx
@@ -3,16 +3,27 @@ import { FormControlBase } from '../forms/FormControl';
 import FormControlWrapper from '../forms/FormControlWrapper';
 import { Box } from '@mui/material';
 import { Button } from '../atoms/Button';
+import * as Yup from 'yup';
 
 const Fields = { STARTDATE: 'startDate', ENDDATE: 'endDate' } as const;
 
 const initialValues = { [Fields.STARTDATE]: '', [Fields.ENDDATE]: '' };
+
+const validationSchema = Yup.object().shape({
+   startDate: Yup.string().required('Start Date is required'),
+   endDate: Yup.string().required('End Date is required'),
+});
+
 type FormValues = typeof initialValues;
 
 export const CustomeDateRangeForm: React.FC<{ onSubmit: (values: FormValues) => void }> = ({ onSubmit }) => {
    return (
       <Box p={2}>
-         <Formik initialValues={initialValues} onSubmit={(values: FormValues) => onSubmit(values)}>
+         <Formik
+            initialValues={initialValues}
+            onSubmit={(values: FormValues) => onSubmit(values)}
+            validationSchema={validationSchema}
+         >
             {(formik) => {
                const isStartDateSelected = !!formik.values[Fields.STARTDATE];
 
@@ -30,11 +41,7 @@ export const CustomeDateRangeForm: React.FC<{ onSubmit: (values: FormValues) => 
                         />
                      </FormControlWrapper>
                      <Box textAlign={'center'} pt={2}>
-                        <Button
-                           color="primary"
-                           type="submit"
-                           disabled={Object.entries(formik.errors).length !== 0 || !formik.isValid}
-                        >
+                        <Button color="primary" type="submit">
                            Submit
                         </Button>
                      </Box>
