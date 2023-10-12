6e36ff38c3afa19b8df47165b8f736334c9740ac
OmololaOlusanya <faroyinomolola@gmail.com>
2023-10-12 11:50:49 +0100

    chore: removed unneeded component from dashboard page

--- src/pages/DashboardOverview.tsx
+++ src/pages/DashboardOverview.tsx
@@ -1,8 +1,6 @@
 import { DashboardHeader } from '@app/components';
-import { NonPerformingLoans } from '@app/components/loan-management/loan-performance/NonPerformingLoans';
-import { PerformingLoans } from '@app/components/loan-management/loan-performance/PerformingLoans';
 import { PageLayout } from '@app/layouts/PageLayout';
 
 export const DashbordOverview = () => {
-   return <PageLayout header={<DashboardHeader />} content={<PerformingLoans />} fullContent={false} />;
+   return <PageLayout header={<DashboardHeader />} content={undefined} fullContent={false} />;
 };
