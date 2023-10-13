f0d059a4b5a796b9764f7b4ccdecf2d5eab271bd
OmololaOlusanya <faroyinomolola@gmail.com>
2023-10-13 14:55:22 +0100

    fix: review suggestions

--- src/pages/LoanPerformance.tsx
+++ src/pages/LoanPerformance.tsx
@@ -1,5 +1,4 @@
 import { PageLayout } from '@app/layouts/PageLayout';
-import { useEffect, useState } from 'react';
 import { BasePath } from '@app/constants';
 import { LoanBookingHeader } from '@app/components/loan-booking/Header';
 import { useSearchParams } from 'react-router-dom';
@@ -8,30 +7,9 @@ import { Grid } from '@mui/material';
 import { SearchLoanProducts } from '@app/components/loan-management/loan-performance/SearchLoanProducts';
 import { PerformanceTable } from '@app/components/loan-management/loan-performance/PerformanceTable';
 
-const LoanPerformanceContent: React.FC<{ getSelectedType: (type: string | null) => void }> = ({
-   getSelectedType,
-}) => {
+export const LoanPerformance = () => {
    const [searchParams] = useSearchParams();
    const type = searchParams.get('type');
-
-   useEffect(() => getSelectedType(type), [type]);
-
-   return (
-      <StyledContentWrapper>
-         <Grid container>
-            <Grid item xs={2.5}>
-               <SearchLoanProducts />
-            </Grid>
-            <Grid item xs={9.5}>
-               <PerformanceTable />
-            </Grid>
-         </Grid>
-      </StyledContentWrapper>
-   );
-};
-
-export const LoanPerformance = () => {
-   const [type, setType] = useState<string | null>('');
 
    return (
       <PageLayout
@@ -42,7 +20,18 @@ export const LoanPerformance = () => {
                pathList={['LOAN MANAGEMENT', 'OVERVIEW', type?.toUpperCase() ?? '']}
             />
          }
-         content={<LoanPerformanceContent getSelectedType={(step) => setType(step)} />}
+         content={
+            <StyledContentWrapper>
+               <Grid container>
+                  <Grid item xs={2.5}>
+                     <SearchLoanProducts />
+                  </Grid>
+                  <Grid item xs={9.5}>
+                     <PerformanceTable />
+                  </Grid>
+               </Grid>
+            </StyledContentWrapper>
+         }
          fullContent={true}
       />
    );
