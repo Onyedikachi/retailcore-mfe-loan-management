559e33bf21da05543a3151a9bf7f5d89b0bf8b91
asiwajudev <olanrewajuranda@gmail.com>
2023-11-26 15:41:32 +0100

    chore: fixed repayment generation with future date bug

--- src/components/forms/DateControl.tsx
+++ src/components/forms/DateControl.tsx
@@ -45,7 +45,7 @@ export const DateControl: React.FC<DateControlProps> = ({
 
    useEffect(() => {
       if (date) {
-         setFieldValue(name, format(date, 'dd/MM/yyyy'));
+         setFieldValue(name, format(date, 'yyyy/MM/dd'));
       }
    }, [date]);
 
@@ -54,7 +54,7 @@ export const DateControl: React.FC<DateControlProps> = ({
    if (props.minDate) {
       minDate = props.maxDate;
    } else if (minDateString) {
-      minDate = parse(minDateString, 'dd/MM/yyyy', new Date());
+      minDate = parse(minDateString, 'yyyy/MM/dd', new Date());
    } else {
       minDate = undefined;
    }
