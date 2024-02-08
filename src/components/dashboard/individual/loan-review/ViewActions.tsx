2f2de3d499164a3e2496c96c889b99f64c6a4599
doyinDev1 <ade.doyin27@gmail.com>
2024-02-08 12:34:09 +0100

    fixed share on loan

--- src/components/dashboard/individual/loan-review/ViewActions.tsx
+++ src/components/dashboard/individual/loan-review/ViewActions.tsx
@@ -6,10 +6,20 @@ import { Previous } from '@app/components/icons/Previous';
 import { printElementContent } from '@app/helper/printTable';
 import { IndividualLoanPath } from '@app/constants/routes';
 import { useNavigate } from 'react-router-dom';
+import { useShareDocument } from '@app/hooks/useShareDocuments';
 
-export const ViewActionButtons = () => {
+interface ViewActionButtonsProps {
+   shareableAreaRef?: React.RefObject<HTMLDivElement>;
+   fileName?: string;
+   htmlElement?: any;
+}
+export const ViewActionButtons = ({ shareableAreaRef, fileName, htmlElement }: ViewActionButtonsProps) => {
    const navigate = useNavigate();
-
+   const { handleShare } = useShareDocument(
+      fileName ?? 'Loan Details',
+      shareableAreaRef,
+      htmlElement
+   );
    return (
       <Box display="flex" alignItems="center" justifyContent="end" px={2}>
          <Button
@@ -20,7 +30,7 @@ export const ViewActionButtons = () => {
          >
             Print
          </Button>
-         <Button sx={{ mx: 2 }} variant="outlined" startIcon={<ShareIcon />}>
+         <Button onClick={handleShare} sx={{ mx: 2 }} variant="outlined" startIcon={<ShareIcon />}>
             Share
          </Button>
          <Button
